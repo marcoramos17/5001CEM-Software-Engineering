e268d308498e535f4a69817a0e077f7b43c1b63e
Marco Ramos <104238008+marcoramos17@users.noreply.github.com>
2023-11-01 12:28:35 +0000

    Refactoring OOP for user accounts

--- project/src/accounts.py
+++ project/src/accounts.py
@@ -2,10 +2,35 @@ class Account:
     """
     Class for Accounts
     """
-    def __init__(self, acc_id, username):
+
+    def __init__(self, username, password, is_new,
+                  role_id = 0, community = 0):
         """
         Constructor for the Account objects
         """
-        self.id = acc_id
         self.username = username
-        print (self.username)
+        
+        if is_new:
+            self.register(role_id, community, password)            
+
+        else:
+            self.login(password)
+            
+
+    def register(self, role_id, community, password):
+        """ user registration function """
+        print("new account - registration")
+        # self.user_id = <last user id in database> + 1
+        # new entry in db; send -> username, pass, user_id, role_id, community
+        self.role_id = role_id
+        self.community = community
+        del password
+
+    def login(self, password):
+        """ user login function """
+        print("existing account - logging in")
+        #self.user_id = <user's id in db>
+        #self.role_id = <user's role id in db>
+        #self.community = <user's community in db>
+        del password
+
--- project/src/main.py
+++ project/src/main.py
@@ -1,4 +1,6 @@
 import accounts
 
 if __name__ == "__main__":
-    new_acc = accounts.Account(1, "TestUser")
+    new_acc = accounts.Account("TestUser", "pass123", False, 1, 1, 1)
+    print(new_acc.username)
+    
