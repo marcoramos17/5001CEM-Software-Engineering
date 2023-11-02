29efb6c66ff18bf3272d751a0d7395bfc73f16d4
Dylan Callaghan <dylanrobcallaghan@gmail.com>
2023-11-02 09:37:15 +0000

    added more examples

--- project/src/database.py
+++ project/src/database.py
@@ -40,33 +40,31 @@ The following variables are available for use upon importing this file. Although
 '''
 db, dbCursor = db_sqlite3_init()
 
-def db_add_school(f_name: str) -> bool:
+def db_add_school(f_name: str) -> None:
     """
     This function is used to add a school to the database.
 
     :param str f_name: Name of the school to be added
-    :return bool: Result
     """
     # We only need the name of the school to add it
     #   since the access code is generated automatically
     dbCursor.execute("INSERT INTO Schools (name) VALUES "
                      "(?)", (f_name,))
     db.commit()
-    return True
+    return
 
-def db_bind_school_teacher(f_username: str) -> bool:
+def db_bind_school_teacher(f_username: str) -> None:
     """
     This function is used to bind a teacher to a school
 
     :param str f_username: Name of the teacher to bind to their stored school
-    :return bool: Result
     """
     schoolID = dbCursor.execute("SELECT Schools_group FROM Accounts WHERE Username = ?", 
                                 (f_username,)).fetchone()[0]
     dbCursor.execute("UPDATE Schools SET teacher = ? WHERE schoolID = ?", 
                      (f_username, schoolID))
     db.commit()
-    return True
+    return
 
 def db_get_teacher_from_username(f_username: str) -> str:
     teacher = dbCursor.execute("SELECT Schools.teacher FROM Accounts "
@@ -80,7 +78,7 @@ def db_create_account(f_firstName: str,
                       f_acesssCode: str,
                       f_dob: str,
                       f_location: str,
-                      f_role: int) -> bool:
+                      f_role: int) -> None:
     """
     This function is used to add an account to the database.
 
@@ -91,7 +89,6 @@ def db_create_account(f_firstName: str,
     :param str dob: Date of birth of the user
     :param str location: Location of the user
     :param int role: The role ID of the user
-    :return bool: Result
     """
     # Use statments on the cursor, remembering to use placeholders (?) to 
     #   prevent against SQL injection attacks
@@ -130,7 +127,7 @@ def db_create_account(f_firstName: str,
         print("Username is: {}\n".format(username))
         print("Salt is: {}\n".format(salt))
 
-    return True
+    return
 
 def db_check_account_login(f_username: str,
                            f_password: str) -> bool:
@@ -148,6 +145,27 @@ def db_check_account_login(f_username: str,
     #   given password is correct. Here we can just return a bool
     return (login[0] == hash_password(f_password, login[1]))
 
+def db_update_password(f_username: str,
+                       f_oldPassword: str,
+                       f_newPassword: str) -> None:
+    """
+    This function is used to change a user's password, it performs a basic password
+    check first.
+
+    :param str f_username: Username to change password for
+    :param str f_oldPassword: Password to compare to stored hash
+    :param str f_newPassword: Password to update to
+    """
+    if not db_check_account_login(f_username, f_oldPassword):
+        raise Exception("Old password incorrect\n")
+    dbCursor.execute(
+        "UPDATE Accounts "
+        "SET password = ? "
+        "WHERE username = ? ", (f_newPassword, f_username)
+    )
+
+
+
 def db_read_account_data(f_username: str) -> tuple [str, 
                                                     str, 
                                                     str, 
@@ -164,7 +182,8 @@ def db_read_account_data(f_username: str) -> tuple [str,
     :param str f_username: Username to read user data for
     :return tuple: Return a tuple containing multiple attributes
     """
-    account = dbCursor.execute("SELECT Accounts.username, "
+    account = dbCursor.execute(
+        "SELECT Accounts.username, "
             "Users.firstName, "
             "Users.lastName, "
             "Users.dob, "
@@ -177,7 +196,8 @@ def db_read_account_data(f_username: str) -> tuple [str,
         "INNER JOIN Accounts ON Accounts.Users_userID = Users.userID "
         "INNER JOIN Schools ON Accounts.Schools_group = Schools.schoolID "
         "INNER JOIN Roles ON Accounts.Roles_roleID = Roles.roleID "
-        "WHERE Accounts.username = ?", (f_username,)).fetchone()
+        "WHERE Accounts.username = ?", (f_username,)
+    ).fetchone()
     return account 
 
 def db_print_account_data(f_username: str) -> None:
@@ -186,7 +206,9 @@ def db_print_account_data(f_username: str) -> None:
 
     :param str f_username: Username to print account data for
     """
+    # Read account data tuple
     loggedIn = db_read_account_data(f_username)
+    # Print string, format with tuple indexes (like list)
     print("Username: {}\n"
         "First Name: {}\n"
         "Last Name: {}\n"
@@ -207,7 +229,7 @@ def db_print_account_data(f_username: str) -> None:
 
 def db_send_message(f_userFrom: str,
                     f_userTo: str,
-                    f_body) -> bool:
+                    f_body) -> None:
     """
     This function is used to send a message between two users in the database.
 
@@ -216,63 +238,174 @@ def db_send_message(f_userFrom: str,
     :param str f_body: Body of the message
     :return bool: Result
     """
+    # Insert data into table
     dbCursor.execute("INSERT INTO Messages ("
                         "Accounts_senderID, "
                         "accounts_recieverID, "
                         "body) "
                     "VALUES (?, ?, ?)", (f_userFrom, f_userTo, f_body))
+    # Ensure that changes are committed to the DB
     db.commit()
-    return True
+    return
     
+def db_get_inbox_users(f_username: str) -> list[tuple[str]]:
+    inboxList = dbCursor.execute(
+        "SELECT DISTINCT Accounts_senderID "
+        "FROM Messages "
+        "WHERE Accounts_recieverID = ? "
+        "UNION "
+        "SELECT DISTINCT Accounts_recieverID "
+        "FROM Messages "
+        "WHERE Accounts_senderID = ?", (f_username, f_username)).fetchall()
+    return inboxList
 
-def db_read_messages_between(f_userFrom: str, f_userTo: str) -> tuple [str, str, str]:
-    messages = dbCursor.execute("SELECT Accounts_senderID, Accounts_recieverID, body "
-                     "FROM Messages "
-                     "WHERE Accounts_senderID = ? OR "
-                        "(Accounts_recieverID = ? AND Accounts_senderID = ?)", 
-                        (f_userFrom, f_userFrom, f_userTo)).fetchall()
+def db_read_messages_between(f_userFrom: str, f_userTo: str) -> list [tuple [str, str, str, str]]:
+    messages = dbCursor.execute(
+        "SELECT Accounts_senderID, Accounts_recieverID, body, timestamp "
+        "FROM Messages "
+        "WHERE Accounts_senderID = ? OR "
+            "(Accounts_recieverID = ? AND Accounts_senderID = ?)", 
+        (f_userFrom, f_userFrom, f_userTo)).fetchall()
     return messages
+
 
 if __name__ == "__main__":
     try:
+        # ----------------------------------------------------------------------
         # Create an account examples
-        # db_create_account('Jimmy', 'Cricket', 'ilovepeas', '582874', '2020-12-30', 'London', 1)
-        # db_create_account('Teacher', 'Covson', 'coventryadmin', '582874', '2000-05-12', 'London', 2)
+        # ----------------------------------------------------------------------
+        # db_create_account('Jimmy', 
+        #                   'Cricket', 
+        #                   'ilovepeas', 
+        #                   '582874', 
+        #                   '2020-12-30', 
+        #                   'London', 
+        #                   1)
+        # db_create_account('Teacher', 
+        #                    'Covson', 
+        #                    'coventryadmin', 
+        #                    '582874', 
+        #                    '2000-05-12', 
+        #                    'London', 
+        #                    2)
+        # db_create_account('Elizabeth',
+        #                     'Brummie',
+        #                     'iLoveBirmingham!',
+        #                     '829854',
+        #                     '2000-11-23',
+        #                     'Birmingham',
+        #                     1)
+        # db_create_account('James',
+        #                     'Dudley',
+        #                     'teachingKidz',
+        #                     '829854',
+        #                     '1978-02-27',
+        #                     'Birmingham',
+        #                     2)
+        # db_bind_school_teacher('JameDudl829854')
 
+        # ----------------------------------------------------------------------
         # Check username/password combinations example
+        # ----------------------------------------------------------------------
         # Correct password
         # print(db_check_account_login('JimmCric582874', 'ilovepeas'))
         # Incorrect password
         # print(db_check_account_login('JimmCric582874', 'ilovepeasWRONG'))
 
+        # ----------------------------------------------------------------------
         # Add school example
+        # ----------------------------------------------------------------------
         # db_add_school("London Modern")
 
+        # ----------------------------------------------------------------------
         # Bind teacher to school example
+        # ----------------------------------------------------------------------
         # db_bind_school_teacher("TeacCovs582874")
         # Read account data example
         # loggedIn = db_read_account_data("JimmCric582874")
 
+        # ----------------------------------------------------------------------
         # Print account data example (debug)
+        # ----------------------------------------------------------------------
         # db_print_account_data("JimmCric582874")
         # db_print_account_data("TeacCovs582874")
 
+        # ----------------------------------------------------------------------
         # Get teacher from username example
+        # ----------------------------------------------------------------------
         # print(db_get_teacher_from_username("JimmCric582874"))
 
+        # ----------------------------------------------------------------------
         # Send message example
-        # db_send_message("TeacCovs582874", "JimmCric582874", "Test message to s1")
+        # ----------------------------------------------------------------------
+        # db_send_message("TeacCovs582874", 
+        #                 "JimmCric582874", 
+        #                 "Test message to s1")
         # time.sleep(1)
-        # db_send_message("JimmCric582874", "TeacCovs582874", "Test message to t2")
+        # db_send_message("JimmCric582874", 
+        #                 "TeacCovs582874", 
+        #                 "Test message to t2")
         # time.sleep(1)
-        # db_send_message("TeacCovs582874", "JimmCric582874", "Test message to s2")
+        # db_send_message("TeacCovs582874", 
+        #                 "JimmCric582874", 
+        #                 "Test message to s2")
+        # db_send_message("JameDudl829854", 
+        #                 "ElizBrum829854", 
+        #                 "Hey student, bham?")
+        # time.sleep(1)
+        # db_send_message("ElizBrum829854", 
+        #                 "JameDudl829854", 
+        #                 "Yes teach, brummie")
+        # time.sleep(1)
+        # db_send_message("JameDudl829854", 
+        #                 "ElizBrum829854", 
+        #                 "No way stu, das crazy")
+        # time.sleep(1)
+        # db_send_message("TeacCovs582874", 
+        #                 "ElizBrum829854", 
+        #                 "hey student from another school!")
 
+        # ----------------------------------------------------------------------
         # Print messages between example
-        messages = db_read_messages_between("TeacCovs582874", "JimmCric582874")
-        for message in messages:
-            print("FROM: {}\nTO:   {}\nBODY: {}\n\n".format(message[0], 
-                                                            message[1], 
-                                                            message[2]))
+        # ----------------------------------------------------------------------
+        # print("Messages between {} and {}".format("TeacCovs582874", "JimmCric582874"))
+        # messages = db_read_messages_between("TeacCovs582874", "JimmCric582874")
+        # for message in messages:
+        #     print("FROM: {}\nTO:   {}\nBODY: {}\n".format(message[0], 
+        #                                                   message[1], 
+        #                                                   message[2]))
+
+        # print("Messages between {} and {}".format("JameDudl829854", "ElizBrum829854"))
+        # messages = db_read_messages_between("JameDudl829854", "ElizBrum829854")
+        # for message in messages:
+        #     print("FROM: {}\nTO:   {}\nBODY: {}\nAT:   {}\n".format(message[0], 
+        #                                                             message[1], 
+        #                                                             message[2],
+        #                                                             message[3]))
+    
+        # ----------------------------------------------------------------------
+        # Show inbox users example
+        # ----------------------------------------------------------------------
+        # inbox = db_get_inbox_users("TeacCovs582874")
+        # print("TeacCovs582874's Inbox:")
+        # for username in inbox:
+        #     print("- " + username[0])
+        # print('')
+        # inbox = db_get_inbox_users("JimmCric582874")
+        # print("JimmCric582874's Inbox:")
+        # for username in inbox:
+        #     print("- " + username[0])
+        # print('')
+        
+
+
+
+
+
+
+        # ----------------------------------------------------------------------
+        # 
+        # ----------------------------------------------------------------------
         None
     except Exception as err:
         print("ERROR:\n", err)
