db902de7f75d55c523ba5103c9abb9c3204b3cf6
Jack-M1 <114680493+Jack-M1@users.noreply.github.com>
2023-11-03 09:59:14 +0000

    2FA Code
    Working functions for each authentication method

---
+++ project/src/GoogleAuthentication.py
@@ -0,0 +1,26 @@
+import pyotp
+import qrcode
+import qrcode.image.svg
+
+# https://pyauth.github.io/pyotp/#module-pyotp
+# Documentation above is key
+# There will need to be database implementation to save the account name and their related RNGSecretTOTP to check later on during log in
+
+
+def userAccountCheck(accountName):
+    RNGSecretGeneration = pyotp.random_base32()
+    RNGSecretTOTP = pyotp.TOTP(RNGSecretGeneration) 
+    print(RNGSecretTOTP) 
+    qrCodeImage = qrcode.make(pyotp.totp.TOTP(RNGSecretGeneration).provisioning_uri(name=accountName, issuer_name="Komodo Hub"), image_factory=qrcode.image.svg.SvgImage)
+    with open('qrcode.svg', 'wb') as qr:
+        qrCodeImage.save(qr)
+    checkLoop = True
+    while (checkLoop == True):
+        check = input("Enter code\n")
+        if check.replace(" ", "") == RNGSecretTOTP.now():
+            print("Code is acceptable, your account has been verified\n")
+            checkLoop = False
+        else:
+            print("Code is not accepted, please try again\n")
+
+userAccountCheck("Jack")
