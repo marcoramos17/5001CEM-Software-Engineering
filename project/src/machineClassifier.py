c485c590182094df335ea7c8d97803e387661491
dylancallaghan <dylanrobcallaghan@gmail.com>
2023-11-09 02:29:41 +0000

    updated working model + file, before changing training params

--- project/src/machineClassifier.py
+++ project/src/machineClassifier.py
@@ -1,16 +1,18 @@
 from PIL import Image
 import tensorflow as tf
-from keras import models
-from keras.layers import Conv2D,\
-                         MaxPooling2D,\
-                         Dense,\
-                         Flatten,\
-                         Dropout
+import matplotlib.pyplot as plt
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.layers import Conv2D,\
+                                           MaxPooling2D,\
+                                           Dense,\
+                                           Flatten,\
+                                           Dropout
 import numpy as np
 import os
 import cv2
 
 rootDir = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
+animalsDir = rootDir + "\\data\\animals\\"
 
 # Only run if the data has been cached previously
 animals = np.load(rootDir + "\\data\\animalCache.npy")
@@ -45,6 +47,7 @@ np.save(rootDir + "\\data\\animalCache",animals)
 np.save(rootDir + "\\data\\labelsCache",labels)
 '''
 
+'''
 # Get the size of the dataset, and the number of different animals
 classesAmount = len(animalNames)
 dataSize    = len(animals)
@@ -52,7 +55,9 @@ dataSize    = len(animals)
 # Split data into test and train
 (x_train, x_test) =  animals[(int)(0.1 * dataSize):],\
                      animals[:(int)(0.1 * dataSize)]
-
+# Split labels into test and train 
+(y_train, y_test) =  labels[(int)(0.1*dataSize):],\
+                     labels[:(int)(0.1*dataSize)]
 x_train = x_train.astype('float32') / 255
 x_test  = x_test.astype('float32')  / 255
 
@@ -60,17 +65,17 @@ x_test  = x_test.astype('float32')  / 255
 train_length = len(x_train)
 test_length  = len(x_test)
 
-# Split labels into test and train 
-(y_train, y_test) =  labels[(int)(0.1*dataSize):],\
-                     labels[:(int)(0.1*dataSize)]
+
+
 
 # Change from integer classes to binary matrices, better for training since the-
 # re is no ordering between the class labels\
 y_train = tf.keras.utils.to_categorical(y_train, classesAmount)
 y_test  = tf.keras.utils.to_categorical(y_test,  classesAmount)
+'''
 
 # # Use the below code if training hasn't been performed yet
-# trainingModel = models.Sequential()
+# trainingModel = Sequential()
 # trainingModel.add(Conv2D(filters=16,kernel_size=2,padding="same",activation="relu",input_shape=(50,50,3)))
 # trainingModel.add(MaxPooling2D(pool_size=2))
 # trainingModel.add(Conv2D(filters=32,kernel_size=2,padding="same",activation="relu"))
@@ -97,26 +102,42 @@ y_test  = tf.keras.utils.to_categorical(y_test,  classesAmount)
 # trainingModel.save(rootDir + "\\data\\model.keras")
 
 # Use if model has been trained already
-trainingModel = models.load_model(rootDir + "\\data\\model.keras")
-score = trainingModel.evaluate(x_test, y_test)
-print('\n', 'Test accuracy:', score)
 
-def convert_to_array(img):
-    im = cv2.imread(img)
-    img = Image.fromarray(im, 'RGB')
-    image = img.resize((50, 50))
+trainingModel = tf.keras.models.load_model(rootDir + "\\data\\model.keras")
+
+def image_to_array(f_imagePath: str) -> np.ndarray:
+    cv2Image = cv2.imread(f_imagePath)
+    pilImage = Image.fromarray(cv2Image, 'RGB')
+    image = pilImage.resize((50, 50))
     return np.array(image)
 
-def predict_animal(file):
-    print("Predicting .................................")
-    ar=convert_to_array(file)
-    ar=ar/255
-    a=[]
-    a.append(ar)
-    a=np.array(a)
-    score=trainingModel.predict(a,verbose=1)
-    label_index=np.argmax(score)
-    acc=np.max(score)
-    print("The predicted Animal is a "+animalNames[label_index]+" with accuracy =    "+str(acc))
+def predict_animal_from_file(f_filePath: str) -> tuple[str, str]:
+    imageArray  = image_to_array(f_filePath)
+    imageArray  = imageArray / 2552
+    imageArray  = np.array([imageArray])
+    score       = trainingModel.predict(imageArray) #type: ignore
+    label_index = np.argmax(score)
+    acc         = np.max(score)
+    return animalNames[label_index], str(acc)
 
-predict_animal(rootDir + "\\data\\animals\\coyote\\0a151f5c78.jpg")
+def show_scaled_image(f_filePath: str) -> None:
+    imageArray  = image_to_array(f_filePath)
+    plt.figure()
+    plt.imshow(imageArray)
+    plt.colorbar()
+    plt.grid(False)
+    plt.show()
+
+def make_animal_prediction_to_user(f_filePath: str) -> tuple[str, str]:
+    prediction, accuracy = predict_animal_from_file(f_filePath)
+    if float(accuracy) >= 75:
+        return prediction, accuracy
+    elif float(accuracy) >= 50:
+        return prediction, "Medium"
+    else:
+        return prediction, "Uncertain"
+
+
+if __name__ == "__main__":
+    animal, acc = predict_animal_from_file(animalsDir + "koala\\2f07008106.jpg")
+    print("Prediction: {}\nAccuracy: {}".format(animal, acc)) 
