35cb2d8799918ed33ad8dea578558c0c10c7b2ef
Marco Ramos <104238008+marcoramos17@users.noreply.github.com>
2023-11-09 11:20:01 +0000

    Collective accounts preparation

--- project/src/main.py
+++ project/src/main.py
@@ -14,6 +14,18 @@ if __name__ == "__main__":
     logging_acc = accounts.Account(False,
                                    password="pass123",
                                    username="PeteSmit829854")
+
+    new_biz = accounts.CollectiveAccount(True,
+                                         password="testpass123",
+                                         fst_name="Yardbird",
+                                         lst_name="Co.",
+                                         date_birth="2020-10-17",
+                                         location="Hub")
+    print("usrnm: \t",new_biz.username)
+
+    logging_biz = accounts.CollectiveAccount(False,
+                                   password="testpass123",
+                                   username="PeteSmit829854")
     #biz_acc = accounts.CollectiveAccount(True, "pass345", "Jimmy",
     #                           "J. Junior", "829854", "Yardbird")
     #print("usrnm: \t",biz_acc.username)
