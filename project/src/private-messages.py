1e3d8523be69d424903b02638c2a678cbdb89c8f
Marco Ramos <104238008+marcoramos17@users.noreply.github.com>
2023-11-17 09:52:32 +0000

    gui button initial set up

--- project/src/private-messages.py
+++ project/src/private-messages.py
@@ -1,7 +1,10 @@
 from guizero import App, Text
 
-app = App(title="Hello world")
+def change_message():
+    message.value = "Not working you silly goose"
 
+app = App(title="Hello world")
 message = Text(app, text="Welcome to the Hello world app!")
+button = PushButton(app, text="Home Page", command=change_message)
 
 app.display()
